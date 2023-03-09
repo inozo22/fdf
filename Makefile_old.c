81fec067b06a78fa875be5f45e74566fe6579aa1
inozo22 <119419194+inozo22@users.noreply.github.com>
2023-03-09 11:27:36 +0000

    compi mlxle

---
+++ Makefile_old.c
@@ -0,0 +1,84 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: nimai <nimai@student.42urduliz.com>        +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2023/02/13 13:02:59 by nimai             #+#    #+#              #
+#    Updated: 2023/02/27 16:54:59 by nimai            ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+NAME = fdf
+
+SRCDIR = ./src/
+SRC = main.c \
+#		all_free.c \
+		cmd1.c \
+		cmd2.c \
+		errors.c \
+		init_ps.c \
+		ps_atoi.c \
+		push_swap.c \
+		quick_sort.c \
+		sort_less6.c \
+		sort_over5.c \
+		sort_over5_2.c \
+		order_cmd.c \
+		print_answer.c \
+		main.c#
+
+OBJDIR = ./obj/
+OBJ = $(addprefix $(OBJDIR), $(SRC:.c=.o))
+
+LIBDIR = ./lib/libft
+
+MLXDIR = ./lib/mlx
+
+CC = gcc
+CFLAGS = -Wall -Wextra -Werror -g #-03
+
+endef
+export sign_fdf
+
+all: $(OBJDIR) $(NAME)
+
+$(OBJDIR):
+	@mkdir -p $@
+
+$(OBJDIR)%.o : $(SRCDIR)%.c
+	$(CC) $(CFLAGS) -c -o $@ $<
+
+$(NAME): $(OBJ)
+	@echo "$(BLUE)--Compiling $(RM_CL) ${YELLOW}$(NAME) $(RM_CL)..."
+	make --directory $(LIBDIR)
+#	@echo "$(GREEN)$(LIBDIR) did$(RM_CL)"
+	$(CC) $(CFLAGS) -I../includes -o $@ $^ -L $(LIBDIR) -lft
+#	@echo "$(GREEN)$(LIBDIR) compiled$(RM_CL)"
+	make --directory $(MLXDIR)
+#	@echo "$(GREEN)$(MLXDIR) did$(RM_CL)"
+#	$(CC) $(CFLAGS) -I/usr/include -I$(MLXDIR) -03 -c $< -o $@
+#	clang *.c -I$(MLX_DIR) -L$(MLX_DIR) -lmlx -lXext -lX11 -lbsd -lm -o cub3D
+	$(CC) $(CFLAGS) -I $(MLXDIR) -lmlx -L/usr/lib -L$(MLXDIR) -lXext -lX11 -lm -lz -o $(NAME)
+
+#	@echo "$(GREEN)$(NAME) created[0m ✔️"
+#	@echo "$$sign_fdf"
+#Name the static library with -lft#
+
+clean:
+	@rm -rf $(OBJDIR)
+	@make clean --directory $(LIBDIR)
+	@make clean --directory $(MLXDIR)
+	@ echo "$(RED)Deleted $(YELLOW)$(NAME) $(RM_CL)objs ✔️"
+
+fclean: clean
+	@make fclean --directory $(LIBDIR)
+	@make fclean --directory $(MLXDIR)
+	@rm -rf ./fdf.dSYM
+	@rm -f $(NAME)
+	@echo "$(RED)Deleted $(YELLOW)$(NAME) $(RM_CL)binary ✔️"
+
+re: fclean all
+
+.PHONY: all, clean, fclean, re
--- main.c
+++ main.c
@@ -1,24 +1,44 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fdf.h                                              :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nimai <nimai@student.42urduliz.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/06 20:54:56 by nimai             #+#    #+#             */
-/*   Updated: 2023/03/08 16:47:12 by nimai            ###   ########.fr       */
+/*   Created: 2023/03/06 20:50:40 by nimai             #+#    #+#             */
+/*   Updated: 2023/03/09 10:45:49 by nimai            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FDF_H
-# define FDF_H
+#include "fdf.h"
 
-# include "../lib/libft/libft.h"
-# include <unistd.h>
-# include <fcntl.h>
-# include <stdio.h>
-# include "../lib/mlx/mlx.h"
+//#include <stdlib.h>
+//#include <mlx.h>
 
-int	main(int ac, char **av);
+int main(void)
+{
+	void	*mlx_ptr;
 
-#endif
+	mlx_ptr = mlx_init();
+	mlx_destroy_display(mlx_ptr);
+	free(mlx_ptr);
+}
+
+/* int	main(int ac, char **av)
+{
+	char	*str;
+	int		fd;
+
+	str = " ";
+	if (ac != 2)
+		return (0);
+	fd = open(av[1], O_RDONLY);
+	while (str)
+	{
+		str = get_next_line(fd);
+		printf("%s", str);
+		free(str);
+	}
+	close(fd);
+	return (0);
+} */
