a58baf54eee3e594fae31261933962c0a82bd288
nimai <119419194+inozo22@users.noreply.github.com>
2023-03-11 11:41:24 +0100

    .

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: nimai <nimai@student.42urduliz.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 20:50:40 by nimai             #+#    #+#             */
-/*   Updated: 2023/03/11 11:37:19 by nimai            ###   ########.fr       */
+/*   Updated: 2023/03/11 11:41:15 by nimai            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,23 +49,3 @@ int	main(void)
 	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
 }
-
-/* int	main(int ac, char **av)
-{
-	char	*str;
-	int		fd;
-
-	str = " ";
-	if (ac != 2)
-		return (0);
-	fd = open(av[1], O_RDONLY);
-	while (str)
-	{
-		str = get_next_line(fd);
-		printf("%s", str);
-		free(str);
-	}
-	close(fd);
-	return (0);
-}
- */
