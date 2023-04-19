b7d082e68ef334efc90e9eb1b5f166ec1316fe50
nimai <119419194+inozo22@users.noreply.github.com>
2023-04-19 17:37:40 +0200

    Without memory leaks

--- lib/libft/ft_split.c
+++ lib/libft/ft_split.c
@@ -11,7 +11,6 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>//kesu
 
 int	word_nbr(char const *str, char c)
 {
@@ -63,12 +62,11 @@ char	**ft_split(char const *s, char c)
 
 	i = 0;
 	tab = (char **) malloc(sizeof(char *) * (word_nbr(s, c) + 1));
-	printf("Line: %d, word_nbr: %d\n", __LINE__, word_nbr(s, c));
 	if (!tab)
 		return (NULL);
 	while (*s && *s == c)
 		s++;
-	while (*s)
+	while (*s && *s != 10)
 	{
 		len = word_len(s, c) + 1;
 		tab[i] = (char *)malloc(sizeof(char *) * len);
@@ -79,10 +77,8 @@ char	**ft_split(char const *s, char c)
 			s++;
 		while (*s == c && *s)
 			s++;
-		printf("Line: %d, tab[%zu]: %p, str: %s\n", __LINE__, i, tab[i], tab[i]);
 		i++;
 	}
 	tab[i] = NULL;
-	printf("Line: %d, tab[%zu]: %p\n", __LINE__, i, tab[i]);
 	return (tab);
 }
--- src/all_free.c
+++ src/all_free.c
@@ -6,7 +6,7 @@
 /*   By: nimai <nimai@student.42urduliz.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 15:11:18 by nimai             #+#    #+#             */
-/*   Updated: 2023/04/19 12:55:57 by nimai            ###   ########.fr       */
+/*   Updated: 2023/04/19 17:29:26 by nimai            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,19 +25,7 @@ char	**strs_clear(char **tab, int i)
 
 void	all_free(t_f *f)
 {
-	printf("Line: %d, f->map->vars.win: %p\n", __LINE__, &(f->map->vars.win));
-	printf("f->map->vars: %p\n", &(f->map->vars));
-	printf("f->map->data: %p\n", &(f->map->data));
-	printf("Line: %d, f->map->data.endian: %p\n", __LINE__, &(f->map->data.endian));
-	printf("f->map->mag: %p\n", &(f->map->mag));
-	printf("f->map: %p\n", &(f->map));
-	printf("f->n[0][0]: %p\n", (f->n[0][0]));
-	printf("f->strs: %p\n", (f->strs));
 	if (&(f->map->vars))
 		free (&(f->map->vars));
-/* 	if (&(f->map->data))
-		free (&(f->map->data)); */
-	
-
 	free(f);
 }
